706120d4ffb96eb089e284d90aa6c01e9939f616
rawkintrevo <trevor.d.grant@gmail.com>
2016-07-31 19:08:16 -0500

    [MAHOUT-1856] ML Algo Framework

---
+++ algos/src/main/scala/org/apache/mahout/algos/transformer/Transformer.scala
@@ -0,0 +1,49 @@
+/**
+  * Licensed to the Apache Software Foundation (ASF) under one
+  * or more contributor license agreements. See the NOTICE file
+  * distributed with this work for additional information
+  * regarding copyright ownership. The ASF licenses this file
+  * to you under the Apache License, Version 2.0 (the
+  * "License"); you may not use this file except in compliance
+  * with the License. You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing,
+  * software distributed under the License is distributed on an
+  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  * KIND, either express or implied. See the License for the
+  * specific language governing permissions and limitations
+  * under the License.
+  */
+package scala.org.apache.mahout.algos.transformer
+
+//import org.apache.mahout.math._
+import org.apache.mahout.math.drm
+import org.apache.mahout.math.drm.DrmLike
+import org.apache.mahout.math.scalabindings._
+
+import scala.reflect.ClassTag
+
+abstract class Transformer extends Serializable {
+  /**
+    * A method to train the model on a given Drm
+    * @param features - Drm of Festures to Train on
+    * @tparam K - Type of values in Drm
+    */
+  def fit[Int](input: DrmLike[Int])
+
+  /**
+    * Transform given Drm given the feature set
+    * @param features
+    * @tparam K
+    */
+  def transform[K: ClassTag](input: DrmLike[K]): DrmLike[K]
+
+  /**
+    * Return a summary of the fit model
+    */
+  def summary(): String
+
+
+}

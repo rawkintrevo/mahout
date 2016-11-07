ccb5da65330e394763928f6dc51d96e38debe4fb
rawkintrevo <trevor.d.grant@gmail.com>
2016-11-07 12:47:55 -0600

    Added MeanSubtract

---
+++ algos/src/main/scala/org/apache/mahout/algos/transformer/MeanCenter.scala
@@ -0,0 +1,54 @@
+package org.apache.mahout.algos.transformer
+
+import org.apache.mahout.math.drm
+import org.apache.mahout.math.scalabindings._
+import org.apache.mahout.math.scalabindings.RLikeVectorOps
+import org.apache.mahout.math.{Vector => MahoutVector}
+import org.apache.mahout.math.scalabindings.RLikeOps._
+import org.apache.mahout.math.scalabindings._
+import org.apache.mahout.math.scalabindings.RLikeVectorOps
+import org.apache.mahout.math.scalabindings.RLikeMatrixOps
+import org.apache.mahout.math.scalabindings.MatrixOps
+import org.apache.mahout.math._
+import org.apache.mahout.math.scalabindings._
+import org.apache.mahout.math.drm._
+import org.apache.mahout.math.scalabindings.RLikeOps._
+import org.apache.mahout.math.drm.RLikeDrmOps._
+import org.apache.mahout.sparkbindings._
+import org.apache.mahout.math.Matrix
+
+import collection._
+import JavaConversions._
+import scala.org.apache.mahout.algos.transformer.Transformer
+import scala.reflect.ClassTag
+
+class SubtractMean extends Transformer {
+
+  def fit[Int](input: DrmLike[Int]) = {
+    fitParams("colMeansV") = input.colMeans
+  }
+
+  def transform[K: ClassTag](input: DrmLike[K]): DrmLike[K] = {
+
+    if (!isFit) {
+      //throw an error
+    }
+
+    implicit val ctx = input.context
+    val colMeansV = fitParams.get("colMeansV").get
+    val bcastV = drmBroadcast(colMeansV)
+
+    val output = input.mapBlock(input.ncol) {
+      case (keys, block) =>
+        val copy: Matrix = block.cloned
+        copy.foreach(row => row -= bcastV.value)
+        (keys, copy)
+    }
+    output
+  }
+
+  def summary(): String = {
+    "not implemented yet"
+  }
+
+}
